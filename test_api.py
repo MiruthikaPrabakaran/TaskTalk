c672fac3abbc1a125f1854c99dffab385f5f09cf
MiruthikaPrabakaran <miruthikaprab3003@gmail.com>
2026-09-05 12:30:31 +0530

    Complete CRUD API

--- test_api.py
+++ test_api.py
@@ -2,12 +2,7 @@ import requests
 
 url = "http://127.0.0.1:5000/api/tasks/1"
 
-task = {
-    "title": "Complete TaskTalk API",
-    "priority": "NORMAL"
-}
-
-response = requests.put(url, json=task)
+response = requests.delete(url)
 
 print(response.status_code)
 print(response.json())
